025fa58c620415ad18c861763177d2339eae8545
Nancy Jackson <nbjackson23@gmail.com>
2020-09-10 19:34:42 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend Metadancing
+  
   include FancyDance :: InstanceMethods
   extend FancyDance :: ClassMethods
   include Dance
