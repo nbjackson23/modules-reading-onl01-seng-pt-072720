86581cdd68e84e82fc0b2f81fb1eafa7470dcad5
Nancy Jackson <nbjackson23@gmail.com>
2020-09-10 19:30:12 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
+class Dancer
+
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  
+  
+  
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+
+  
+end
+
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
+class Kid
+  extend Metadancing
+  include FancyDance :: InstanceMethods
+  extend FancyDance :: ClassMethods
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
+  
+end
+
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
